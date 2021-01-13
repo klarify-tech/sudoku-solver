db92cd6d39f547b3105ef924315c5eabeed543c9
klarify-tech <imac@IMACs-iMac.local>
2021-01-13 21:30:16 +0530

    Just comments

---
+++ sudoku.py
@@ -0,0 +1,13 @@
+#Klarify.tech
+#We will build a console based sudoku solver
+
+# Define a board 9X9 
+# Import the incoming board
+
+#Three important functions
+
+#Check vertical
+#Check Horizontal
+#Check subsqaure
+
+
